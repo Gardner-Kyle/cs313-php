b54bb2cb593ee8927b8550efd4d04333818b69ac
Kyle Gardner <kb_gardner@icloud.com>
2020-04-28 16:25:22 -0600

    all but stretch completed

--- web/week2.js
+++ web/week2.js
@@ -7,3 +7,9 @@ function changeColor() {
 	var color = document.getElementById("colorText").value
 	div1.style.backgroundColor = color
 }
+
+$(document).ready(function(){
+  $("#colorBtn").click(function(){
+		$("#div1").css("background-color", "blue")
+  });
+});
