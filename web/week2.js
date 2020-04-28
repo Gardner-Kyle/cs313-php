8ca0fe7cc4b2fd5a18a76463c6c2977c40cd3921
Kyle Gardner <kb_gardner@icloud.com>
2020-04-28 15:28:55 -0600

    got a little mixed up haha

--- web/week2.js
+++ web/week2.js
@@ -1,3 +1,9 @@
 function clicked() {
 	window.alert("Clicked!");
 }
+
+function changeColor() {
+	var div1 = document.getElementById("div1");
+	var color = document.getElementById("colorText").value
+	div1.style.backgroundColor = color
+}
