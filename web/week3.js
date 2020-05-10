7a5bb8a57f8018048915e0431dd05d5c87c9202f
Kyle Gardner <kb_gardner@icloud.com>
2020-05-09 19:15:03 -0600

    working on week3

--- web/week3.js
+++ web/week3.js
@@ -5,25 +5,25 @@ var img4 = 0
 
 function img1btn() {
 	var val = document.getElementById("btn1");
-	this.img1 += 1
+	img1 += 1
 	val.innerHTML = "+" + img1;
 }
 
 function img2btn() {
 	var val = document.getElementById("btn2");
-	this.img2 += 1
+	img2 += 1
 	val.innerHTML = "+" + img2;
 }
 
 function img3btn() {
 	var val = document.getElementById("btn3");
-	this.img3 += 1
+	img3 += 1
 	val.innerHTML = "+" + img3;
 }
 
 function img4btn() {
 	var val = document.getElementById("btn4");
-	this.img4 += 1
+	img4 += 1
 	val.innerHTML = "+" + img4;
 }
 
