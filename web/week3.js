8afac284cb8d224341e976467921d70dde0bbc17
Kyle Gardner <kb_gardner@icloud.com>
2020-05-09 19:36:09 -0600

    working on week3

--- web/week3.js
+++ web/week3.js
@@ -1,51 +1,51 @@
-var img1 = 0
-var img2 = 0
-var img3 = 0
-var img4 = 0
+var img1: number = 0;
+var img2 = 0;
+var img3 = 0;
+var img4 = 0;
 
 function img1btn() {
 	var val = document.getElementById("btn1");
-	this.img1 += 1
+	this.img1 += 1;
 	val.innerHTML = "+" + img1;
 }
 
 function img2btn() {
 	var val = document.getElementById("btn2");
-	this.img2 += 1
+	this.img2 += 1;
 	val.innerHTML = "+" + img2;
 }
 
 function img3btn() {
 	var val = document.getElementById("btn3");
-	this.img3 += 1
+	this.img3 += 1;
 	val.innerHTML = "+" + img3;
 }
 
 function img4btn() {
 	var val = document.getElementById("btn4");
-	this.img4 += 1
+	this.img4 += 1;
 	val.innerHTML = "+" + img4;
 }
 
 function submit() {
 	if (this.img1 < 1) {
 		var val = document.getElementById("ch_img1");
-		val.style.visibility = 'hidden';
+		val.style.display = 'none';
 		val.innerHTML = img1;
 	}
 	if (this.img2 < 1) {
 		var val = document.getElementById("ch_img2");
-		val.style.visibility = 'hidden';
+		val.style.display = 'none';
 		val.innerHTML = "+" + img2;
 	}
 	if (this.img3 < 1) {
 		var val = document.getElementById("ch_img3");
-		val.style.visibility = 'hidden';
+		val.style.display = 'none';
 		val.innerHTML = img3;
 	}
 	if (this.img4 < 1) {
 		var val = document.getElementById("ch_img4");
-		val.style.visibility = 'hidden';
+		val.style.display = 'none';
 		val.innerHTML = "yup";
 	}
 }
