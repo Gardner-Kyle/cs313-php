d3633ebef0131ddef2c2fb489c26d16fc0e83e96
Kyle Gardner <kb_gardner@icloud.com>
2020-05-23 17:46:49 -0600

    working on php request

--- web/get.php
+++ web/get.php
@@ -1,7 +1,8 @@
 <?php
 	$userPassword = $_POST["password"];
 	echo "Password: $userPassword<br>";
-	$db_connection = pg_connect("host=localhost dbname=quotes");
+	$db_connection = pg_connect("host=localhost port=5432 dbname=quotes user= password="
+);
 
 	if (!$db_connection) {
 		echo "FAILURE<br>";
