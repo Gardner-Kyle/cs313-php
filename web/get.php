6b0d1ce26219d35eebb047df3ebdd4cd41a4665d
Kyle Gardner <kb_gardner@icloud.com>
2020-05-23 17:12:37 -0600

    working on php request

--- web/get.php
+++ web/get.php
@@ -1,8 +1,13 @@
 <?php
 	$userPassword = $_POST["password"];
 	echo "Password: $userPassword<br>";
-	pg_connect("host=localhost dbname=quotes");
-	echo "DB: $db_connection<br>";
+	$db_connection = pg_connect("host=localhost dbname=quotes user= password= ");
+
+	if ($db_connection) {
+		echo "FAILURE";
+	} else {
+		echo "SUCCESS";
+	}
 	$result = pg_query("SELECT * FROM quotes.quotes.user");
 	echo "User Data: $result";
 ?>
