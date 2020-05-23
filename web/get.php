9ddf103ad16ecbc7a7b56c1fbf7285726c9a3a65
Kyle Gardner <kb_gardner@icloud.com>
2020-05-23 17:18:15 -0600

    working on php request

--- web/get.php
+++ web/get.php
@@ -8,6 +8,8 @@
 	} else {
 		echo "SUCCESS<br>";
 		echo "DB Name: " . pg_dbname($db_connection) . "<br>";
+		echo "HOSTNAME: " . pg_host($db_connection) . "<br>";
+		echo "PORT: " . pg_port($db_connection) . "<br>";
 	}
 	$result = pg_query("SELECT id FROM quotes.quotes.user");
 	echo "User Data: $result";
