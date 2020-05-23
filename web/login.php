4b1acc0ff2ac427e7b1372c13760e63cec3849f3
Kyle Gardner <kb_gardner@icloud.com>
2020-05-23 16:40:32 -0600

    working on php request

--- web/login.php
+++ web/login.php
@@ -1,26 +1,27 @@
 <!DOCTYPE html>
 <html>
-	<head>
+<head>
+<script src="db.js" type="text/javascript"></script>
+<link rel="stylesheet" type="text/css" href="week2.css">
+</head>
+<body>
 
-	</head>
-	<body>
+<?php
+	$userPassword = $_POST["password"]
+	$db_connection = pg_connect("host=localhost dbname=quotes user= password=");
+	$result = pg_query($db_connection, "SELECT * FROM user WHERE user.password = ");
+	echo "User Data: \n$result"
+?>
 
-		<?php
-			$userPassword = $_POST["password"]
-    		$db_connection = pg_connect("host=localhost dbname=quotes user= password=");
-    		$result = pg_query($db_connection, "SELECT * FROM user WHERE user.password = ");
-    		echo "User Data: \n$result"
-		?>
-
-		<div class="div" id="div1"> Welcome <br>
+<div class="div" id="div1"> Welcome <br>
+</div>
+<div>
+	<form action="<?php $this-> ?>" method="post">
+		<div class="div" id="div2"> 
+			<input type="text" name="password">
 		</div>
-		<div>
-			<form action="<?php $this-> ?>" method="post">
-				<div class="div" id="div2"> 
-					<input type="text" name="password">
-				</div>
-				<button type="submit" style="margin-top: 10%" id="submit">Login</button>
-			</form>
-		</div>
-	</body>
+		<button type="submit" style="margin-top: 10%" id="submit">Login</button>
+	</form>
+</div>
+</body>
 </html>
