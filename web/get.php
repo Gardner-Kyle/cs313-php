5016a9d3df35817afb553980942165bbdae5c061
Kyle Gardner <kb_gardner@icloud.com>
2020-05-23 17:37:04 -0600

    working on php request

--- web/get.php
+++ web/get.php
@@ -1,12 +1,7 @@
 <?php
-	$host        = "host=127.0.0.1";                         
-	$port        = "port=5432";                        
-	$dbname      = "dbname=quotes";                         
-	$credentials = "user='' password=''";
-
 	$userPassword = $_POST["password"];
 	echo "Password: $userPassword<br>";
-	$db_connection = pg_connect("$host $port $dbname $credentials");
+	$db_connection = pg_connect("host=localhost dbname=quotes user='' password=''");
 
 	if (!$db_connection) {
 		echo "FAILURE<br>";
@@ -15,7 +10,8 @@
 		echo "DB Name: " . pg_dbname($db_connection) . "<br>";
 		echo "HOSTNAME: " . pg_host($db_connection) . "<br>";
 		echo "PORT: " . pg_port($db_connection) . "<br>";
+
+		$result = pg_query("SELECT id FROM quotes.quotes.user");
+	    echo "User Data: $result";
 	}
-	$result = pg_query("SELECT id FROM quotes.quotes.user");
-	echo "User Data: $result";
 ?>
