babba28e9d1f87912e66df4887c7e6a3c1d5a8bd
Kyle Gardner <kb_gardner@icloud.com>
2020-05-23 16:25:00 -0600

    working on php request

--- web/login.php
+++ web/login.php
@@ -6,6 +6,12 @@
 	</head>
 	<body>
 
+		<?php
+			$userPassword = $_POST["password"]
+    		$db_connection = pg_connect("host=localhost dbname=quotes user= password=");
+    		$result = pg_query($db_connection, "SELECT * FROM user WHERE user.password = ");
+    		echo "User Data: \n$result"
+		?>
 
 		<div class="div" id="div1"> Welcome <br>
 		</div>
