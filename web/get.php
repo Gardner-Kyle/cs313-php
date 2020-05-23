d6aba8cf22bf760ecff40571e0b0bd2c252739fc
Kyle Gardner <kb_gardner@icloud.com>
2020-05-23 17:22:06 -0600

    working on php request

--- web/get.php
+++ web/get.php
@@ -1,7 +1,15 @@
 <?php
+	display_errors = On
+	display_startup_errors = On
+
+	$host        = "host=127.0.0.1";                         
+	$port        = "port=5432";                        
+	$dbname      = "dbname=quotes";                         
+	$credentials = "user= password=";
+
 	$userPassword = $_POST["password"];
 	echo "Password: $userPassword<br>";
-	$db_connection = pg_connect("host=localhost dbname=quotes user=root password=root ");
+	$db_connection = pg_connect("host=localhost dbname=quotes user=root password=root");
 
 	if ($db_connection) {
 		echo "FAILURE<br>";
