7308e3130f1c90b6b0ffe83ae215c4605292779b
Kyle Gardner <kb_gardner@icloud.com>
2020-05-23 16:48:29 -0600

    working on php request

--- web/login.php
+++ web/login.php
@@ -7,9 +7,7 @@
 <body>
 
 <?php
-	$userPassword = $_POST["password"]
-	$db_connection = pg_connect("host=localhost dbname=quotes user= password=");
-	$result = pg_query($db_connection, "SELECT * FROM user WHERE user.password = ");
+	$userPassword = $_POST["password"];
 	echo "User Data: $result";
 ?>
 
