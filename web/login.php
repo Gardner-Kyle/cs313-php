4fecbbd9ac020ca5444c20d60cda13fa203046f6
Kyle Gardner <kb_gardner@icloud.com>
2020-05-23 16:33:34 -0600

    working on php request

--- web/login.php
+++ web/login.php
@@ -6,15 +6,22 @@
 	</head>
 	<body>
 
-	
+		<?php
+			$userPassword = $_POST["password"]
+    		$db_connection = pg_connect("host=localhost dbname=quotes user= password=");
+    		$result = pg_query($db_connection, "SELECT * FROM user WHERE user.password = ");
+    		echo "User Data: \n$result"
+		?>
 
 		<div class="div" id="div1"> Welcome <br>
 		</div>
-		<form action="<?php $this-> ?>" method="post">
-			<div class="div" id="div2"> 
-				<input type="text" name="password">
-			</div>
-			<button type="submit" style="margin-top: 10%" id="submit">Login</button>
-		</form>
+		<div>
+			<form action="<?php $this-> ?>" method="post">
+				<div class="div" id="div2"> 
+					<input type="text" name="password">
+				</div>
+				<button type="submit" style="margin-top: 10%" id="submit">Login</button>
+			</form>
+		</div>
 	</body>
 </html>
