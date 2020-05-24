9e187bac898381f9631070b1fec843fd4cd8f9c2
Kyle Gardner <kb_gardner@icloud.com>
2020-05-23 18:25:55 -0600

    working on php request

--- web/get.php
+++ web/get.php
@@ -1,7 +1,7 @@
 <?php
 	$userPassword = $_POST["password"];
 	echo "Password: $userPassword<br>";
-	$db_connection = pg_connect("host=localhost dbname=quotes");
+	$db_connection = pg_connect("host=localhost port=5432 dbname=quotes user=kylegardner");
 
 	if (!$db_connection) {
 		echo "FAILURE<br>";
