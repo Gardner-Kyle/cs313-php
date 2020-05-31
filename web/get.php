86c6bb57a032c540c4efb7e82961eb4a10ff18e1
Kyle Gardner <kb_gardner@icloud.com>
2020-05-30 18:07:48 -0600

    trying to connect

--- web/get.php
+++ web/get.php
@@ -1,17 +1,15 @@
 <?php
 	$userPassword = $_POST["password"];
 	echo "Password: $userPassword<br>";
-	$db_connection = pg_connect("host=localhost port=5432 dbname=quotes user=kylegardner password=3200Perryhill");
 
-	if (!$db_connection) {
-		echo "FAILURE<br>";
-	} else {
+	$dbUrl = “postgres://cseuser:Spring2020@localhost:5432/quotes”
+
+
 		echo "SUCCESS<br>";
-		echo "DB Name: " . pg_dbname($db_connection) . "<br>";
-		echo "HOSTNAME: " . pg_host($db_connection) . "<br>";
-		echo "PORT: " . pg_port($db_connection) . "<br>";
+		echo "DB Name: " . pg_dbname($dbUrl) . "<br>";
+		echo "HOSTNAME: " . pg_host($dbUrl) . "<br>";
+		echo "PORT: " . pg_port($dbUrl) . "<br>";
 
 		$result = pg_query("SELECT id FROM quotes.quotes.user");
 	    echo "User Data: $result";
-	}
 ?>
