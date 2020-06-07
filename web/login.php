dbf65f14c52dd9155a6b14f393c3a97518ce6770
Kyle Gardner <kb_gardner@icloud.com>
2020-06-06 20:22:36 -0600

    Working on final

--- web/login.php
+++ web/login.php
@@ -11,8 +11,8 @@
 				<div class="div" id="div2"> 
 					<input placeholder="Username" type="text" name="username">
 					<input placeholder="Password" type="text" name="password">
+					<button type="submit" style="margin-top: 10%" id="submit">Login</button>
 				</div>
-				<button type="submit" style="margin-top: 10%" id="submit">Login</button>
 			</form>
 		</div>
 	</body>
