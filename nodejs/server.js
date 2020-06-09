bb8079a1f65a568564558090a3b804214716f5f7
Kyle Gardner <kb_gardner@icloud.com>
2020-06-08 20:11:35 -0600

    week8 server

--- nodejs/server.js
+++ nodejs/server.js
@@ -3,9 +3,6 @@ const http = require('http');
 const requestListener = function (req, res) {
   res.writeHead(200);
   res.end('Hello, World!');
-
-  req.writeHead(404);
-  req.end('Page Not Found');
 }
 
 const server = http.createServer(requestListener);
