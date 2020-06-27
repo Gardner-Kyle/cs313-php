4d15ea633291c27e0972f102eb6e5bf8829c6e49
Kyle Gardner <kb_gardner@icloud.com>
2020-06-27 15:37:01 -0600

    projw1

--- web/db.js
+++ web/db.js
@@ -1,21 +1,23 @@
-const http = require('http');
+function server() {
+  const http = require('http');
 
-http.createServer((request, response) => {
-  request.on('error', (err) => {
-    console.error(err);
-    response.statusCode = 400;
-    response.end();
-  });
-  response.on('error', (err) => {
-    console.error(err);
-  });
-  if (request.method === 'POST') {
-    request.pipe(response);
-  } else {
-    response.statusCode = 404;
-    response.end();
-  }
-}).listen(8080);
+  http.createServer((request, response) => {
+    request.on('error', (err) => {
+      console.error(err);
+      response.statusCode = 400;
+      response.end();
+    });
+    response.on('error', (err) => {
+      console.error(err);
+    });
+    if (request.method === 'POST') {
+      request.pipe(response);
+    } else {
+      response.statusCode = 404;
+      response.end();
+    }
+  }).listen(8080);
+}
 
 function connect() {
   var con = pg.createConnection({
