b0b719c13fc0f0b1b129d4d7de01b976425b9c36
Kyle Gardner <kb_gardner@icloud.com>
2020-06-27 15:35:27 -0600

    projw1

--- web/db.js
+++ web/db.js
@@ -16,3 +16,20 @@ http.createServer((request, response) => {
     response.end();
   }
 }).listen(8080);
+
+function connect() {
+  var con = pg.createConnection({
+    host: "localhost",
+    user: "kylegardner",
+    password: "",
+    database: "quotes"
+  });
+
+  con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT * FROM user", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+    });
+  });
+}
